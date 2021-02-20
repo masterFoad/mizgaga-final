e61ae796e3610c24f098f88ef565a9ef48e8290c
Foad Abo Dahood <foad.a@taboola.com>
2021-02-21 01:57:20 +0200

    - added db schema

--- mizgaga-app/finalMizgaga/src/main/resources/schema-mysql.sql
+++ mizgaga-app/finalMizgaga/src/main/resources/schema-mysql.sql
@@ -1,9 +1,21 @@
 create schema if not exists mizgaga_db collate utf8mb4_0900_ai_ci;
 use mizgaga_db;
-create table if not exists faces
+create table faces
 (
     id int auto_increment
         primary key,
     face int null
+);
+
+create table session_vectors
+(
+    id int auto_increment
+        primary key,
+    creation_time timestamp default CURRENT_TIMESTAMP null,
+    actual_time timestamp null,
+    x double null,
+    y double null,
+    z double null,
+    w double null
 );
 
