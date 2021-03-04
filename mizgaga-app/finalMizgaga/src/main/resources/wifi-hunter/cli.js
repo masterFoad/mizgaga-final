c14b59a5cb824899454f33f0bb404d6e23223977
Foad Abo Dahood <foad.a@taboola.com>
2021-03-04 14:24:28 +0200

    fixed bug in wifi hunter such that it won't connect the laptop if the sensor is already connected

--- mizgaga-app/finalMizgaga/src/main/resources/wifi-hunter/cli.js
+++ mizgaga-app/finalMizgaga/src/main/resources/wifi-hunter/cli.js
@@ -19,48 +19,56 @@ wifi.scan((error, networks) => {
 
         const isNetworkFound = networks.find(network => network.ssid === args[0]);
         if (isNetworkFound) {
-            networks.filter(network => network.ssid === 'config_me!').forEach(network => {
-                wifi.connect({ssid: 'config_me!'}, error => {
-                    if (error) {
-                        if (error.toString().includes("Duplicate Content-Length")) {
-                            console.log("success");
-                        } else {
-                            console.log('failed to connect to config_me!');
-                            console.log(error);
-                        }
-                    }
 
-                    if (args && args[0] && args[1]) {
-                        fetch(`http://192.168.4.1/wifisave?s=${encodeURIComponent(args[0])}&p=${encodeURIComponent(
-                            args[1])}`, {
-                                  method: 'get',
-                                  headers: {
-                                      // 'Content-Type': 'text/html'
-                                      'Content-Type': 'application/x-www-form-urlencoded',
-                                  },
-                              }).then(res => {
-                            wifi.connect({ssid: args[0], password: args[1]}, error => {
-                                console.log("connected to the same wifi as the sensor");
-                                console.log("success");
-                            });
-                        })
-                            .catch(err => {
-                                if (err.toString().includes("Duplicate")) {
-                                    console.log("sensor is successfully connected to " + args[0]);
-                                    wifi.connect({ssid: args[0], password: args[1]}, error => {
-                                        console.log("connected to the same wifi as the sensor " + args[0]);
-                                        console.log("success");
-                                    });
-                                } else {
-                                    console.log('failed to connect to config_me!');
-                                    console.log(err);
-                                }
-                            });
-                    }
-
-                    // console.log('Connected');
+            if (networks.filter(network => network.ssid === 'config_me!').length === 0) {
+                wifi.connect({ssid: args[0], password: args[1]}, error => {
+                    console.log("connected to the same wifi as the sensor " + args[0]);
+                    console.log("success");
                 });
-            });
+            } else {
+                networks.filter(network => network.ssid === 'config_me!').forEach(network => {
+                    wifi.connect({ssid: 'config_me!'}, error => {
+                        if (error) {
+                            if (error.toString().includes("Duplicate Content-Length")) {
+                                console.log("success");
+                            } else {
+                                console.log('failed to connect to config_me!');
+                                console.log(error);
+                            }
+                        }
+
+                        if (args && args[0] && args[1]) {
+                            fetch(`http://192.168.4.1/wifisave?s=${encodeURIComponent(args[0])}&p=${encodeURIComponent(
+                                args[1])}`, {
+                                      method: 'get',
+                                      headers: {
+                                          // 'Content-Type': 'text/html'
+                                          'Content-Type': 'application/x-www-form-urlencoded',
+                                      },
+                                  }).then(res => {
+                                wifi.connect({ssid: args[0], password: args[1]}, error => {
+                                    console.log("connected to the same wifi as the sensor");
+                                    console.log("success");
+                                });
+                            })
+                                .catch(err => {
+                                    if (err.toString().includes("Duplicate")) {
+                                        console.log("sensor is successfully connected to " + args[0]);
+                                        wifi.connect({ssid: args[0], password: args[1]}, error => {
+                                            console.log("connected to the same wifi as the sensor " + args[0]);
+                                            console.log("success");
+                                        });
+                                    } else {
+                                        console.log('failed to connect to config_me!');
+                                        console.log(err);
+                                    }
+                                });
+                        }
+
+                        // console.log('Connected');
+                    });
+                });
+            }
         } else {
             console.log("requested network not found");
         }
